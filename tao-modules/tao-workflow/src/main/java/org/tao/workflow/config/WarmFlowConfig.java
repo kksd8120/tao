aadd6e6ad1fad441c8acdac17c9cefe225748b6d
kksd8120 <elay_china@163.com>
2025-04-21 14:23:38 +0800

    init

---
+++ tao-modules/tao-workflow/src/main/java/org/tao/workflow/config/WarmFlowConfig.java
@@ -0,0 +1,16 @@
+package org.tao.workflow.config;
+
+import org.tao.workflow.common.ConditionalOnEnable;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * warmFlow配置
+ *
+ * @author may
+ */
+@ConditionalOnEnable
+@Configuration
+public class WarmFlowConfig {
+
+}
+
---
+++ tao-modules/tao-workflow/src/main/java/org/tao/workflow/mapper/TestLeaveMapper.java
@@ -0,0 +1,15 @@
+package org.tao.workflow.mapper;
+
+import org.tao.common.mybatis.core.mapper.BaseMapperPlus;
+import org.tao.workflow.domain.TestLeave;
+import org.tao.workflow.domain.vo.TestLeaveVo;
+
+/**
+ * 请假Mapper接口
+ *
+ * @author may
+ * @date 2023-07-21
+ */
+public interface TestLeaveMapper extends BaseMapperPlus<TestLeave, TestLeaveVo> {
+
+}
